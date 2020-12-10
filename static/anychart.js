ea1f92f886e88a10e5486cd2a79da4dd28f06b1c
Jeff Lyng <jely94@icloud.com>
2020-12-10 17:20:50 -0600

    added code

--- static/anychart.js
+++ static/anychart.js
@@ -4,7 +4,6 @@ function myFunction() {
 
     // Clear map container
     document.getElementById("container").innerHTML = ""; 
-     
 
     //#container-map.html("");
     // load the data
@@ -13,6 +12,7 @@ function myFunction() {
 
         // Variable to hold the data
         var geoData = data
+        var hairData = data
 
         console.log(geoData)
 
@@ -94,5 +94,233 @@ function myFunction() {
     });
 
 
+//};
+
+
+
+//SCATTER CODE**********************************************************
+
+
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
 };
 
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "GDP";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(hairData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(hairData, d => d[chosenXAxis]) * 0.8,
+      d3.max(hairData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+  var label;
+
+  if (chosenXAxis === "GDP") {
+    label = "GDP:";
+  }
+  else {
+    label = "Life_Exp:";
+  }
+
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.Country}<br>${label} ${d[chosenXAxis]}`);
+    });
+
+  circlesGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+  })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+      toolTip.hide(data);
+    });
+
+  return circlesGroup;
+}
+
+// Retrieve data from the CSV file and execute everything below
+d3.json(`/data/${x}`, function(err, hairData) { 
+// d3.csv("hairData.csv").then(function(hairData, err) {
+  if (err) throw err;
+  console.log(hairData);
+
+
+//hairData = geoData
+  // parse data
+  hairData.forEach(function(data) {
+    data.GDP = +data.GDP;
+    data.Happiness_Score = +data.Happiness_Score;
+    data.Life_Exp = +(data['Life Expectancy']);
+  });
+
+  // xLinearScale function above csv impocrt
+  var xLinearScale = xScale(hairData, chosenXAxis);
+
+  // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(hairData, d => d.Happiness_Score)])
+    .range([height, 0]);
+
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // append initial circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(hairData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d.Happiness_Score))
+    .attr("r", 8)
+    .attr("stroke", "#0000ff")
+    .attr("stroke-width", 3)
+    .attr("fill", "#ff00ff")
+    .attr("opacity", ".25");
+
+  // Create group for two x-axis labels
+  var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+  var GDP_Label = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("value", "GDP") // value to grab for event listener
+    .classed("active", true)
+    .text("GDP per Capita");
+
+  var LifeExp_Label = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 40)
+    .attr("value", "Life_Exp") // value to grab for event listener
+    .classed("inactive", true)
+    .text("Life Expectancy");
+
+  // append y axis
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .classed("axis-text", true)
+    .text("Happiness Score");
+
+  // updateToolTip function above csv import
+  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+  // x axis labels event listener
+  labelsGroup.selectAll("text")
+    .on("click", function() {
+      // get value of selection
+      var value = d3.select(this).attr("value");
+      if (value !== chosenXAxis) {
+
+        // replaces chosenXAxis with value
+        chosenXAxis = value;
+
+        // console.log(chosenXAxis)
+
+        // functions here found above csv import
+        // updates x scale for new data
+        xLinearScale = xScale(hairData, chosenXAxis);
+
+        // updates x axis with transition
+        xAxis = renderAxes(xLinearScale, xAxis);
+
+        // updates circles with new x values
+        circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+
+        // updates tooltips with new info
+        circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+        // changes classes to change bold text
+        if (chosenXAxis === "Life_Exp") {
+          LifeExp_Label
+            .classed("active", true)
+            .classed("inactive", false);
+          GDP_Label
+            .classed("active", false)
+            .classed("inactive", true);
+        }
+        else {
+          LifeExp_Label
+            .classed("active", false)
+            .classed("inactive", true);
+          GDP_Label
+            .classed("active", true)
+            .classed("inactive", false);
+        }
+      }
+    });
+  }).catch(function(error) {
+    console.log(error);
+})}
